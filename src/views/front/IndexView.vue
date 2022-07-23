3822c96829eb1de52447d0d7d7c107fa4f8c83b7
ssuchen <ie5513@hotmail.com>
2022-07-23 15:18:45 +0800

    change

--- src/views/front/IndexView.vue
+++ src/views/front/IndexView.vue
@@ -29,7 +29,7 @@
     <div class="row position-absolute menu-title">
       <div class="col">
         <!-- <img src="" alt="閣樓面包坊" /> -->
-        <h1>THE<span></span>LOTTIC<span></span>BREAD</h1>
+        <h1>THE<span></span>LOTTIC<span></span>11BREAD</h1>
         <p class="subtitle">
           Happiness is not about being immortal nor having food or rights in
           one’s hand、<span></span> It’s about having each tiny wish come true,
